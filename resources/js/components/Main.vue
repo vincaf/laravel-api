57a7050d6db906f8c9054b6c21010b30a332eaee
vincaf <vincenzocafassog@gmail.com>
2022-09-28 00:30:28 +0200

    feat(Main.vue): create Main component

---
+++ resources/js/components/Main.vue
@@ -0,0 +1,21 @@
+<template>
+  <main class="container">
+    <div class="row">
+        <div class="col-12">
+            <h1 class="p-3 m-3">
+                Recent posts:
+            </h1>
+        </div>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
