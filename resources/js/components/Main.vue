0a4fb5214ed6f14de6c4cd44f330ffa5fc59d845
vincaf <vincenzocafassog@gmail.com>
2022-09-28 01:18:43 +0200

    feat(Main.vue): add get posts method from api

--- resources/js/components/Main.vue
+++ resources/js/components/Main.vue
@@ -5,15 +5,48 @@
             <h1 class="p-3 m-3">
                 Recent posts:
             </h1>
+
+            <PostCard />
         </div>
     </div>
   </main>
 </template>
 
 <script>
-export default {
+    import PostCard from '../components/PostCard.vue';
+    import axios from 'axios';
 
-}
+    export default {
+        components: {
+            PostCard,
+        },
+
+        data: function(){
+            return{
+                posts: [],
+                currentPage: 1,
+                lastPage: null,
+                loading: false,
+            }
+        },
+
+        methods: {
+            getPosts(postsPage = 1){
+                axios.get('/api/posts' , {
+                    page: postsPage
+                }).then((response) => {
+                    console.log(response);
+
+                }).catch((error) => {
+                    console.error(error);
+                })
+            }
+        },
+
+        created(){
+            this.getPosts();
+        }
+    }
 </script>
 
 <style>
