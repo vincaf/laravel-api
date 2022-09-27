63740a6a87873354b9b554487e53353d529eb799
vincaf <vincenzocafassog@gmail.com>
2022-09-28 00:26:35 +0200

    feat: removed welcome from guest & create home

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('guest.welcome');
-});
-
 Auth::routes();
 
 Route::middleware('auth')
@@ -30,3 +26,7 @@ Route::middleware('auth')
     });
 
 // Route::middleware('auth')->get('/home', 'Admin\HomeController@index')->name('home');
+
+Route::get("{any?}", function(){
+    return view("guest.home");
+})->where("any", ".*");
