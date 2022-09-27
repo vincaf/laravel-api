a3c821d936d4f8a7f201c8e266e784b58bd99776
vincaf <vincenzocafassog@gmail.com>
2022-09-27 18:12:52 +0200

    feat(api): add route to delete with auth

--- routes/api.php
+++ routes/api.php
@@ -21,4 +21,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::namespace('Api')->group(function(){
     Route::get('/posts', 'PostController@index');
     Route::get('/posts/{id}', 'PostController@show');
+    Route::middleware('auth:api')->delete('/posts/{id}', 'PostController@destroy');
 });
