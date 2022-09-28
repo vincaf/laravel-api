560a42a33de4c227aaa890f179aca97b15561c83
vincaf <vincenzocafassog@gmail.com>
2022-09-28 23:54:25 +0200

    feat(api): add route for TagController index&show

--- routes/api.php
+++ routes/api.php
@@ -22,4 +22,7 @@ Route::namespace('Api')->group(function(){
     Route::get('/posts', 'PostController@index');
     Route::get('/posts/{id}', 'PostController@show');
     Route::middleware('auth:api')->delete('/posts/{id}', 'PostController@destroy');
+
+    Route::get('/tags', 'TagController@index');
+    Route::get('/tags/{id}', 'TagController@show');
 });
